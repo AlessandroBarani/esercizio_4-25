18caa59754f8d43f45f8874f4ce228b1271c32fa
nicob <nicob@MACBOOKPROALE>
2025-03-16 18:09:12 +0100

    Add project files.

---
+++ esercizio_4-25/Program.cs
@@ -0,0 +1,9 @@
+﻿double y;
+Console.WriteLine("Inserisci un numero: ");
+y = double.Parse(Console.ReadLine()); 
+int x = 0;  
+while (Math.Pow(x, x) < y)
+{
+    x++;
+}
+Console.WriteLine($"Il numero trovato è {x-1} che soddisfa {x-1}^{x - 1} < {y}");
